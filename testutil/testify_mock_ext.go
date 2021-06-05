e14cd66b240d51cb5cf1910dbbbade36afb34706
alfian <alfian853@gmail.com>
2021-06-05 17:07:17 +0700

    extend testify's mock implementation

---
+++ testutil/testify_mock_ext.go
@@ -0,0 +1,54 @@
+package testutil
+
+import (
+	"github.com/stretchr/testify/mock"
+	"reflect"
+)
+
+type ModifiedMock struct {
+	mock.Mock
+}
+
+func (m *ModifiedMock) On(methodName string, arguments ...interface{}) *mock.Call {
+
+	lenObs := len(m.ExpectedCalls)
+	var targetIndex *int
+
+	// search and replace existing method mock
+	for i := 0; i < lenObs; i++ {
+		if methodName == m.ExpectedCalls[i].Method {
+			targetIndex = &i
+			break
+		}
+	}
+
+	if targetIndex != nil {
+		if lenObs == 1 {
+			m.ExpectedCalls = m.ExpectedCalls[:0]
+		}
+		lastIndex := lenObs - 1
+
+		// swap target with the last index
+		m.ExpectedCalls[*targetIndex], m.ExpectedCalls[lastIndex] =
+			m.ExpectedCalls[lastIndex], m.ExpectedCalls[*targetIndex]
+
+		m.ExpectedCalls = m.ExpectedCalls[:lastIndex]
+	}
+	//search and replace done
+
+	return m.Mock.On(methodName, arguments...)
+}
+
+// isArgsEqual compares arguments
+func isArgsEqual(expected mock.Arguments, args []interface{}) bool {
+	if len(expected) != len(args) {
+		return false
+	}
+	for i, v := range args {
+		if !reflect.DeepEqual(expected[i], v) {
+			return false
+		}
+	}
+	return true
+}
+
